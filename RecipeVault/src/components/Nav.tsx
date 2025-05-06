a474b11fe2bc8b0ad9f3f2e7d6194bef1f6ddb0f
Jan Schumacher Vincke <janschumacher98@gmail.com>
2025-05-06 13:37:44 +0200

    first try of prisma

--- RecipeVault/src/components/Nav.tsx
+++ RecipeVault/src/components/Nav.tsx
@@ -4,15 +4,26 @@ export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+
   return (
     <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Search</a>
-        </li>
-        <li class={`border-b-2 ${active("/recipes")} mx-1.5 sm:mx-6`}>
-          <a href="/recipes">Recipes</a>
-        </li>
+      <ul class="container flex items-center justify-between p-3 text-gray-200">
+        <div class="flex">
+          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+            <a href="/">Search</a>
+          </li>
+          <li class={`border-b-2 ${active("/recipes")} mx-1.5 sm:mx-6`}>
+            <a href="/recipes">Recipes</a>
+          </li>
+        </div>
+        <div class="flex space-x-4">
+          <button class="bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
+            Sign In
+          </button>
+          <button class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </div>
       </ul>
     </nav>
   );
